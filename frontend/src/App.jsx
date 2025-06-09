aa9b0fd827498bfe5dbc1acfa19a6bed0c5e2eba
maneeq166 <lumpsytuber@gmail.com>
2025-06-09 11:26:20 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,46 @@
-import React from 'react'
-import {BrowserRouter,Route,Routes} from 'react-router-dom'
-import Signin from './pages/Signin'
-import Signup from './pages/Signup'
-import Home from './pages/Home'
-import Profile from './pages/Profile'
-import ProtectedRoutes from "./pages/ProtectedRoutes.jsx"
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import Profile from './pages/Profile';
+import ProtectedRoutes from './pages/ProtectedRoutes.jsx';
+import NotFound from './pages/NotFound.jsx';
+import LayoutWithHeader from './components/LayoutWithHeader.jsx';
+import LayoutWithoutHeader from './components/LayoutWithoutHeader.jsx';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Header from "./components/Header.jsx"
-import NotFound from "./pages/NotFound.jsx"
-
 
 function App() {
   return (
-    <div>
+    <>
       <BrowserRouter>
-      <Header></Header>
-          <Routes>
-              <Route path='/' element={<Home/>}  />
-              <Route path='/signup' element={<Signup/>} />
-              <Route path='/signin' element={<Signin/>} />
-              <Route path='/profile' element={
+        <Routes>
+          {/* Routes WITH header */}
+          <Route element={<LayoutWithHeader />}>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/profile"
+              element={
                 <ProtectedRoutes>
-                  <Profile/>
+                  <Profile />
                 </ProtectedRoutes>
-                } />
+              }
+            />
+          </Route>
 
-              
-                {/* 404 Route */}
-                <Route path="*" element={<NotFound />} />
-          </Routes>
+          {/* Routes WITHOUT header */}
+          <Route element={<LayoutWithoutHeader />}>
+            <Route path="/signin" element={<Signin />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="*" element={<NotFound />} />
+          </Route>
+        </Routes>
       </BrowserRouter>
+
       <ToastContainer position="bottom-right" autoClose={5000} />
-    </div>
-  )
+    </>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/components/LayoutWithoutHeader.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const LayoutWithoutHeader = () => {
+  return <Outlet />;
+};
+
+export default LayoutWithoutHeader;
