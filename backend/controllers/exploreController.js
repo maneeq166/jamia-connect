94e0bd86713fa36eed8fc0211094e3e948c6c345
maneeq166 <lumpsytuber@gmail.com>
2025-07-22 02:56:18 +0530

    commit

--- backend/controllers/exploreController.js
+++ backend/controllers/exploreController.js
@@ -19,34 +19,32 @@ async function getManyUsers(req, res) {
     });
   } catch (error) {
     res.status(500).json({ message: "Server error", success: false });
-    console.log("Error:",error);
-    
+    console.log("Error:", error);
   }
 }
 // app.use("/api/v1/explore", exploreRouter);
-async function getUsers(req,res){
-    try {
-        const username = req.params.username;
+async function getUsers(req, res) {
+  try {
+    const username = req.params.username;
 
-        const user = await User.findOne({username}).select("-password");
+    const user = await User.find({
+      username: { $regex: username, $options: "i" },
+    }).select("-password");
 
-        if(!user){
-           return res.status(404).json({message:"Not found",success:false});
-        }
-
-        console.log(user);
-
-        res.json({user,success:true});
-
-
-        
-
-    } catch (error) {
-        console.error(error);
-    res.status(500).json({ message: "Server error", success: false });
+    if (!user) {
+      return res.status(404).json({ message: "Not found", success: false });
     }
+
+    console.log(user);
+
+    res.json({ user, success: true });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Server error", success: false });
+  }
 }
 
 module.exports = {
-    getManyUsers,getUsers
-}
+  getManyUsers,
+  getUsers,
+};
